9142adbc2cabca0fd6ac32772b677e68cadc871f
Colin Cole <110618749+Ccole888@users.noreply.github.com>
2026-02-27 15:45:47 +0000

    Comments

--- 4helper.c
+++ 4helper.c
@@ -3,22 +3,27 @@
 
 extern unsigned int ram[];
 extern int sum(int size);
-
+//Reads the file name from the command line
 int main(int argc, char* argv[]){
+    //opens file in read mode
     FILE *file = fopen(argv[1], "r");
     int size;
+    //reads first line of the data file and stores as size
     fscanf(file, "%d", &size);
+    //declare an array with size size
     int intlist[size];
+    //adds each line of the data file to the intlist array
     for(int i = 0; i < size; i++){
         fscanf(file, "%*[^\n]");
         fscanf(file, "%*c");
         fscanf(file, "%d", &intlist[i]);
     }
     fclose(file);
+    //puts intlist into ram
     for(int i = 0; i < size; i++){
         ram[i] = intlist[i];
     }
-    
+    //calls sum and prints result and size
     unsigned int result = sum(size);
     printf("Sum: %u\n", result);
     printf("Size: %d\n", size);
--- lab4.s
+++ lab4.s
@@ -1,4 +1,5 @@
 .section .bss
+    #configures ram 
     .lcomm ram, 4096
     .global ram
 
@@ -6,17 +7,20 @@
     .global sum
 
 sum:
+    #zeros eax
     mov $0, %eax
+    #moves arg from c file into rcx
     mov %rdi, %rcx
+    #zeros rdi and rdx
     xor %rdi, %rdi
     xor %rdx, %rdx
 L1:  
+    #adds value at rdx in ram to eax, incriments by 4 because ram is unsinged int and not char
     add ram(, %rdx, 4), %eax
+    #incriements ram index and subtracts count index
     inc %rdx
     dec %rcx
     jnz L1
-
-    mov %eax, ram+0x51
     ret
 
 .section .note.GNU-stack,"",@progbits
